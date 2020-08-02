b7464fa4470aee3b27f504b71bc2b5a6872d0c2a
iuri-pdista <correaiuri86@gmail.com>
2020-08-02 12:14:53 -0300

    Implement libera and atribui functions

--- route/route.c
+++ route/route.c
@@ -13,3 +13,15 @@ bool cria(float x, float y, char* nome){
   if (c == NULL){ return false; }
   return true; 
 }
+
+void libera(Cidade* cidade){
+  free(cidade);
+}
+
+void atribui(Cidade* cidade, float x, float y, char nome[50]){
+  (*cidade).x = x;
+  (*cidade).y = y;
+  (*cidade).nome = nome;
+  printf("Os novos dados da Cidade sao: \n Nome: %s \n  Coordenadas: (%.0f %.0f)", nome, x, y);
+}
+
