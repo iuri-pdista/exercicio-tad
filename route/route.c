ba3cf682358a6663fe66cc6c65e0f6fd2f0621d2
iuri-pdista <correaiuri86@gmail.com>
2020-08-02 12:32:17 -0300

    Implement distancia function

--- route/route.c
+++ route/route.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
+#include<math.h>
 
 #include<point.h>
 
@@ -25,3 +26,24 @@ void atribui(Cidade* cidade, float x, float y, char nome[50]){
   printf("Os novos dados da Cidade sao: \n Nome: %s \n  Coordenadas: (%.0f %.0f)", nome, x, y);
 }
 
+float * distancia(Cidade * c1, Cidade * c2){
+  float distacias[3];
+
+  float distanciaHorizontal;
+  if (((*c1).x - (*c2).x) < 0 ){
+    distanciaHorizontal = (*c2).x - (*c1).x; 
+  }
+  else { distanciaHorizontal = (*c1).x - (*c2).x; }
+ 
+  float distanciaVertical;                         
+  if (((*c1).y - (*c2).y) < 0 ){
+    distanciaVertical = (*c2).y - (*c1).y; 
+  }
+  else { distanciaVertical = (*c1).y - (*c2).y; }
+  
+  float distanciaReal = sqrt(( pow( distanciaHorizontal, 2 ) + pow( distanciaVertical , 2 )));
+  distancias[0] = distanciaReal;
+  distancias[1] = disatanciaHorizontal;
+  distancias[2] = distanciaVertical;
+  return distancias;
+} 
