d449a3cd7e1a4c5d50a28d8c68b90b7ebdeca3d9
iuri-pdista <correaiuri86@gmail.com>
2020-08-02 11:43:00 -0300

    First Commit :tada:

---
+++ route/point.h
@@ -0,0 +1,33 @@
+#ifndef PONTO_H_INCLUDED
+#define PONTO_H_INCLUDED
+struct cidade {
+	float x;
+	float y;
+	char nome[50];
+};
+/* TAD: Cidade (x,y, nome) */
+/* Tipo exportado */
+typedef struct cidade Cidade;
+/* Funções exportadas */
+/* Função cria
+ * ** Aloca e retorna uma cidade com coordenadas (x,y) e nome
+ * */
+Cidade* cria (float x, float y, char * nome);
+/* Função libera
+ * ** Libera a memória de uma cidade previamente criada.
+ * */
+void libera (Cidade* c);
+/* Função acessa
+ * ** Devolve os valores das coordenadas e nome de uma cidade
+ *
+ * */
+void acessa (Cidade * c, float* x, float* y, char * nome);
+/* Função atribui
+ * ** Atribui novos valores às coordenadas e nome de uma cidade
+ * */
+void atribui (Cidade * c, float x, float y, char nome[50]);
+/* Função distancia
+ * ** Retorna a distância entre duas cidades
+ * */
+float distancia (Cidade * c1, Cidade * c2);
+#endif // PONTO_H_INCLUDED
