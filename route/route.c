bfc050a216819d0f1589a6bb6592d04e8ca13711
iuri-pdista <correaiuri86@gmail.com>
2020-08-02 11:50:02 -0300

    Implemented Cria function

--- route/route.c
+++ route/route.c
@@ -1,3 +1,13 @@
-int main(){
-  return 0;
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+#include<point.h>
+
+Cidade* cria(float x, float y, char* nome){
+  Cidade* c = (Cidade *) malloc(sizeof(Cidade));
+  strcpy_s((*c).nome, 100, nome);
+  (*c).x = x;
+  (*c).y = y;
+  return c;
 }
