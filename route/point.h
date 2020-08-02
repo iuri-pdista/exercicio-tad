cc908d7cbd04269a9eee78419ed0bd7311ac9a80
iuri-pdista <correaiuri86@gmail.com>
2020-08-02 12:19:31 -0300

    Change point.h function's signature

--- route/point.h
+++ route/point.h
@@ -16,18 +16,18 @@ bool cria (float x, float y, char * nome);
 /* Função libera
  * ** Libera a memória de uma cidade previamente criada.
  * */
-void libera (Cidade* c);
+void libera (Cidade* cidade);
 /* Função acessa
  * ** Devolve os valores das coordenadas e nome de uma cidade
  *
  * */
-void acessa (Cidade * c, float* x, float* y, char * nome);
+void acessa (Cidade * cidade, float* x, float* y, char * nome);
 /* Função atribui
  * ** Atribui novos valores às coordenadas e nome de uma cidade
  * */
-void atribui (Cidade * c, float x, float y, char nome[50]);
+void atribui (Cidade * cidade, float x, float y, char nome[50]);
 /* Função distancia
  * ** Retorna a distância entre duas cidades
  * */
-float distancia (Cidade * c1, Cidade * c2);
+float * distancia (Cidade * c1, Cidade * c2);
 #endif // PONTO_H_INCLUDED
